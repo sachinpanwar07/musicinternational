b895a7bfcf58f0f382a68dcd7fcffe26824442e7
TusharDwivedi04 <tushardwivedi04may@gmail.com>
2024-10-26 13:45:37 +0530

    Changes

--- src/screens/Members.js
+++ src/screens/Members.js
@@ -1,11 +1,13 @@
 
 import React from 'react';
+import "../Style/members.css";
 
 const Members = () => {
   return (
     <section id="members">
       <h2>Members</h2>
       <p>Meet our dedicated members who contribute to the music legacy.</p>
+      
     </section>
   );
 };
