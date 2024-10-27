7e6d7a366380da1207d0c4d93f1155a28085bd9b
sachin Panwar <sachinpanwar2037@gmail.com>
2024-10-27 15:18:15 +0530

    changes in members by tushar_dev

--- src/constants/imagePath.js
+++ src/constants/imagePath.js
@@ -2,5 +2,5 @@ export default{
     ic_logo:require('../constants/images/mli.jpeg'),
     ic_homeimg:require('../constants/images/backimage.jpg'),
     ic_homebg:require('../constants/images/homebg.jpg'),
-    ic_singer:require('../constants/images/singer.png')
+    ic_singer:require('../constants/images/person.jpg')
 }
