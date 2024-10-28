e4c7e5060ab5e4941d9ac0ad5d60087d80b86e2b
sachin Panwar <sachinpanwar2037@gmail.com>
2024-10-28 19:03:20 +0530

    login fix deploye issue

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,37 +1,37 @@
 import React from "react";
-import '../Style/login.css'
+import '../Style/login.css'; 
 import imagePath from "../constants/imagePath";
+
 const Login = () => {
   return (
     <section id="login" className="login-container">
-  <div className="login-section">
-    <div className="login-header">
-      <h2>Sign In</h2>
-      <div className="login-social">
-        <img src={imagePath.ic_facebookicon} alt="Facebook" className="social-icon"/>
-        <img src={imagePath.ic_xicon} alt="Twitter" className="social-icon"/>
+      <div className="login-section">
+        <div className="login-header">
+          <h2>Sign In</h2>
+          <div className="login-social">
+            <img src={imagePath.ic_facebookicon} alt="Facebook" className="social-icon"/>
+            <img src={imagePath.ic_xicon} alt="Twitter" className="social-icon"/>
+          </div>
+        </div>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" placeholder="Username" />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" placeholder="Password" />
+        <button className="login-btn">Sign In</button>
+        <div className="remember-forgot">
+          <label>
+            <input type="checkbox" /> Remember Me
+          </label>
+          <a href="#">Forgot Password</a>
+        </div>
       </div>
-    </div>
-    <label htmlFor="username">Username</label>
-    <input type="text" id="username" placeholder="Username" />
-    <label htmlFor="password">Password</label>
-    <input type="password" id="password" placeholder="Password" />
-    <button className="login-btn">Sign In</button>
-    <div className="remember-forgot">
-      <label>
-        <input type="checkbox" /> Remember Me
-      </label>
-      <a href="#">Forgot Password</a>
-    </div>
-  </div>
 
-  <div className="login-welcome-section">
-    <h2>Welcome to login</h2>
-    <p>Don't have an account?</p>
-    <a href="#" className="signup-btn">Sign Up</a>
-  </div>
-</section>
-
+      <div className="login-welcome-section">
+        <h2>Welcome to login</h2>
+        <p>Don't have an account?</p>
+        <a href="#" className="signup-btn">Sign Up</a>
+      </div>
+    </section>
   );
 };
 
